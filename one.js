11f4f57bcd52c66dcb40183633cd824fdae384e3
Raselraaz123 <82328669+Raselraaz123@users.noreply.github.com>
2022-02-20 11:12:32 +0600

    after day end

---
+++ one.js
@@ -0,0 +1,81 @@
+
+
+//    // aftre day
+// function ass(num1, num2) {
+//     return num1*num2
+// }
+// const total=ass(30, 50)
+// console.log(total)
+  
+
+// const add = function asst(g1, g2) {
+//     return g1/g2
+// }
+// const total2 = add(50, 2);
+// console.log(total2);
+
+
+// const number = function too(go1, fh) {
+//     return go1-fh
+// }
+// const booo = number(99, 90);
+// console.log(booo);
+
+
+// const rot = function (boo, nan) {
+//     return boo +nan
+// }
+// const rej = rot(50, 30)
+// console.log(rej);
+
+// // default value
+// const add4 = (hoo, gooo=60) => hoo + gooo
+// const total4 = add4(40)
+// console.log(total4);
+
+
+// const add5 = (jaa, maa) => jaa - maa;
+// const total6 = add5(50, 30);
+// console.log(total6);
+
+// // arrow function
+const add = (ewo, bo) => ewo * bo;
+const total = add(40, 3);
+// console.log(total);
+ 
+const sum = (hoo, goo) => hoo + goo;
+const num = sum(40, 20);
+// console.log(num);
+
+
+const price = (ram, driv, monetor, dis) => {
+    const sum2 = ram + driv;
+    const sum3 = sum2 * monetor;
+    const sum4 = sum3 - dis
+    return sum4;
+    
+}
+const priceList=price(30,50,2,100)
+console.log(priceList);
+
+const fivetime = (num) => num * 4;
+const total4 = fivetime(50);
+console.log(total4)
+
+const tentime = gooo => gooo * 10
+const total0 = tentime(20);
+console.log(total0)
+
+const number = [59, 30, 40, 598, 10]
+console.log(number)
+console.log(...number)
+
+const max = Math.max(59, 39, 209, 600);
+console.log(max)
+const mai = Math.min(0, 20, 50, 4999, 40)
+console.log(mai)
+
+
+const numbers = [60, 400, 40, 100, 39, 40000];
+console.log(...numbers)
+// after day end
